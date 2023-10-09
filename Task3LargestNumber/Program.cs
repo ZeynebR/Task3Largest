98d6bed321fc3a5e1f20ee9882e4eb563a7e871c
ZeynebR <zeynabnr@code.edu.az>
2023-10-09 08:41:07 +0200

    task3 added

---
+++ Task3LargestNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] Numbers = { 2005,3, 8, 23,106, 1, 55 ,677};
+int LargestNumber = Numbers[0];
+for (int i = 1; i < Numbers.Length; i++)
+{
+    
+    if (LargestNumber < Numbers[i])
+	{
+        LargestNumber = Numbers[i];
+	}
+    
+        
+}
+Console.WriteLine( "largest number is " +LargestNumber);
